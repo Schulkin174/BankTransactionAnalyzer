58cd8d6b22d89684f50b4cf44802591be9b301cf
schulkin.alexander <schulkin.alexander@gmail.com>
2023-12-23 01:46:49 +0300

    v 2-6 использование CSV-парсера и обработка списков транзакций

--- src/main/java/BankTransactionAnalyzerSimple.java
+++ src/main/java/BankTransactionAnalyzerSimple.java
@@ -5,69 +5,42 @@ import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
-
-/*
-Данный код указан в учебнике. Не работает. Возможно, проблема в отсутствии правильного указания аргументов.
-
-public class BankTransactionAnalyzerSimple {
-    private static final String RESOURCES = "src/main/resources/";
-
-    public static void main(String[] args) throws IOException {
-
-      if (args.length == 0) {
-            System.out.println("Please provide the file name as a command line argument");
-            return;
-        }
-
-        final Path path = Paths.get(RESOURCES + args[0]);
-        final List<String> lines = Files.readAllLines(path);
-        double total = 0d;
-        for (final String line: lines) {
-            final String[] columns = line.split(",");
-            final double amount = Double.parseDouble(columns[1]);
-            total += amount;
-        }
-
-        System.out.println("The total for all transactions is " + total);
-    }
-}
- */
 
 
 public class BankTransactionAnalyzerSimple {
     private static final String RESOURCES = "src/main/resources/";
     private static final String FILE_NAME = "transactions"; // Жестко закодированный путь к файлу
 
-    public BankTransactionAnalyzerSimple() throws IOException {
-    }
+    final BankStatementCSVParser bankStatementParser = new BankStatementCSVParser();
 
     public static void main(String[] args) throws IOException {
-
+        final BankTransactionAnalyzerSimple analyzer = new BankTransactionAnalyzerSimple();
         final Path path = Paths.get(RESOURCES, FILE_NAME);
         final List<String> lines = Files.readAllLines(path);
+
+        final List<BankTransaction> bankTransactions = analyzer.bankStatementParser.parseLinesFromCSV(lines);
+
+        System.out.println("The total for all transactions is " + calculateTotalAmount(bankTransactions));
+        System.out.println("Transactions in January " + selectInMonth(bankTransactions, Month.JANUARY));
+    }
+
+    public static double calculateTotalAmount(final List<BankTransaction> bankTransactions) {
         double total = 0d;
-        for (final String line : lines) {
-            final String[] columns = line.split(",");
-            final double amount = Double.parseDouble(columns[1]); //30-01-2017 индекс 1,-50.00 индекс 2,Tesco индекс 3. (parseDouble переформатирует строку в число типа Double)
-            total += amount;
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            total += bankTransaction.getAmount();
         }
+        return total;
+    }
 
-        System.out.println("The total for all transactions is " + total);
-    // } вот здесь по-хорошему завершить и вывести январь в отдельный класс
-
-        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        for (final String line : lines) {
-            final String[] columns = line.split(",");
-            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
-            if (date.getMonth() == Month.JANUARY) {
-                final double amount = Double.parseDouble(columns[1]);
-                total += amount;
+    public static List<BankTransaction> selectInMonth(final List<BankTransaction> bankTransactions, final Month month) {
+        final List<BankTransaction> bankTransactionsInMonth = new ArrayList<>();
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            if (bankTransaction.getDate().getMonth() == month) {
+                bankTransactionsInMonth.add(bankTransaction);
             }
         }
-        System.out.println("The total for all transactions in January is " + total);
+        return bankTransactionsInMonth;
     }
- }
-
-
-
+}
